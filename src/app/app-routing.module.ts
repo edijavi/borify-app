adfb70fd29ecd3a750a28497e3b4cc2c532cca79
Xamp <deivid.develop@gmail.com>
2020-05-15 18:50:14 +0200

    Create route base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { ArtistComponent } from './components/artist/artist.component';
+
+const routes: Routes = [
+  // {path: 'artist', component: ArtistComponent},
 
 
-const routes: Routes = [];
+  // {path: '404', component: Error404Component},
+  // {path: '**', redirectTo: '404'},
+  {path: '', redirectTo: 'artist', pathMatch: 'full'},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
