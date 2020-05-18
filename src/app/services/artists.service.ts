f09da8bf410f21c430ecf1de75fafc51739cbafb
Xamp <deivid.develop@gmail.com>
2020-05-18 03:17:43 +0200

    Added getArtist function

--- src/app/services/artists.service.ts
+++ src/app/services/artists.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Observable } from 'rxjs/internal/Observable';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ArtistsService {
 
-  constructor() { }
+  constructor(private http: HttpClient) {
+
+  }
+
+  getArtist (): Observable <any> {
+      debugger
+      return this.http.get(`${environment.apiBack}/artists`)
+    }
+
 }
