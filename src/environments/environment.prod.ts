8a9eb496980065e5855431757382296cad0183d7
Edison Lamar <edilata@msn.com>
2020-05-20 07:03:05 +0200

    Deploy to heroku front

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  // apiBack: 'http://localhost:3977/api',
+  apiBack: 'https://borify-api-rest.herokuapp.com/api',
+  ip: '127.0.0.1'
 };
